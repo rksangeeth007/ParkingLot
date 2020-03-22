f72e02653c07aedd4d9825aff88be08e6ef12275
Sangeeth Hari Sankar <sharisankar@hotels.com>
2020-03-23 00:21:51 +0530

    Intitial commit

---
+++ src/test/java/com/gojek/parkinglot/ParserTest.java
@@ -0,0 +1,32 @@
+package com.gojek.parkinglot;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.Assert.*;
+
+public class ParserTest {
+    Parser parser = new Parser();
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    @Before
+    public void setUpStreams() {
+        System.setOut(new PrintStream(outContent));
+    }
+
+    @After
+    public void cleanUpStreams() {
+        System.setOut(null);
+    }
+    @Test
+    public void parseTextInput() throws Exception {
+        parser.parseTextInput("hello");
+        assertEquals("Invalidinput", outContent.toString().trim().replace(" ", ""));
+        parser.parseTextInput("status");
+        assertEquals("Invalidinput\nSorry,parkinglotisnotcreated", outContent.toString().trim().replace(" ", ""));
+    }
+
+}
